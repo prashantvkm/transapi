caef36909709e25d8077f275f89ef752e634447e
Prashant Vishwakarma <prashant.vishwakarma@news.co.uk>
2020-02-17 00:52:46 +0530

    initial commit for starter spring boot project

---
+++ src/main/java/com/exman/transapi/TransapiApplication.java
@@ -0,0 +1,13 @@
+package com.exman.transapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TransapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TransapiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/exman/transapi/TransapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.exman.transapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TransapiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
